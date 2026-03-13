d46cdf04efbf397d1b14a34b33e9db875d00bf5b
Sherif80 <sherifashraf1054@gmail.com>
2026-03-13 22:48:49 +0200

    Some Changes with the BookModel to avoid errors while parsing data

--- lib/data/models/book_model/epub.dart
+++ lib/data/models/book_model/epub.dart
@@ -1,16 +1,13 @@
 class Epub {
 	bool? isAvailable;
-	String? downloadLink;
 
-	Epub({this.isAvailable, this.downloadLink});
+	Epub({this.isAvailable});
 
 	factory Epub.fromJson(Map<String, dynamic> json) => Epub(
 				isAvailable: json['isAvailable'] as bool?,
-				downloadLink: json['downloadLink'] as String?,
 			);
 
 	Map<String, dynamic> toJson() => {
 				'isAvailable': isAvailable,
-				'downloadLink': downloadLink,
 			};
 }
