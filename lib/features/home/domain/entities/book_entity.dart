4d00ed9d0521603f14a978834fb714492a663fe7
Sherif80 <sherifashraf1054@gmail.com>
2026-03-13 21:06:26 +0200

    Created the book entity

---
+++ lib/features/home/domain/entities/book_entity.dart
@@ -0,0 +1,15 @@
+class BookEntity {
+  final String name;
+  final String image;
+  final String authorName;
+  final num price;
+  final num rating;
+
+  BookEntity({
+    required this.name,
+    required this.image,
+    required this.authorName,
+    required this.price,
+    required this.rating,
+  });
+}
