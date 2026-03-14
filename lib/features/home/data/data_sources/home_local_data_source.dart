27df8f26cab103e680e92fb69568cfb39d8152af
Sherif80 <sherifashraf1054@gmail.com>
2026-03-14 02:49:38 +0200

    Created home local data source

---
+++ lib/features/home/data/data_sources/home_local_data_source.dart
@@ -0,0 +1,18 @@
+import 'package:bookly/features/home/domain/entities/book_entity.dart';
+
+abstract class HomeLocalDataSource {
+  List<BookEntity> fetchFeaturedBooks();
+  List<BookEntity> fetchNewestBooks();
+}
+
+class HomeLocalDataSourceImpl implements HomeLocalDataSource {
+  @override
+  List<BookEntity> fetchFeaturedBooks() {
+    return [];
+  }
+
+  @override
+  List<BookEntity> fetchNewestBooks() {
+    return [];
+  }
+}
