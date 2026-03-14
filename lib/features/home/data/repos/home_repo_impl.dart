881ba710acb9e18bef74a75b6b33227b1d44661a
Sherif80 <sherifashraf1054@gmail.com>
2026-03-14 03:36:41 +0200

    Created home repo impl in data layer

---
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -0,0 +1,18 @@
+import 'package:bookly/core/errors/failure.dart';
+import 'package:bookly/features/home/domain/entities/book_entity.dart';
+import 'package:bookly/features/home/domain/repos/home_repo.dart';
+import 'package:dartz/dartz.dart';
+
+class HomeRepoImpl implements HomeRepo {
+  @override
+  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks() {
+    // TODO: implement fetchFeaturedBooks
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, List<BookEntity>>> fetchNewestBooks() {
+    // TODO: implement fetchNewestBooks
+    throw UnimplementedError();
+  }
+}
