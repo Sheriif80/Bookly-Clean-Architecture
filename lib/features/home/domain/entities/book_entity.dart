ce3d0c406a877f44c160d787555c452863a82651
Sherif80 <sherifashraf1054@gmail.com>
2026-03-14 03:04:06 +0200

    Generated book entity adapter to use it in hive_ce

--- lib/features/home/domain/entities/book_entity.dart
+++ lib/features/home/domain/entities/book_entity.dart
@@ -1,9 +1,20 @@
+import 'package:hive_ce_flutter/hive_flutter.dart';
+
+part 'book_entity.g.dart';
+
+@HiveType(typeId: 0)
 class BookEntity {
+  @HiveField(0)
   final String bookID;
+  @HiveField(1)
   final String title;
+  @HiveField(2)
   final String? image;
+  @HiveField(3)
   final String? authorName;
+  @HiveField(4)
   final num? price;
+  @HiveField(5)
   final num? rating;
 
   BookEntity({
