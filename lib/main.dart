4aa4a99b728faecd2475f98fc7d6ca2cbac6904b
Sherif80 <sherifashraf1054@gmail.com>
2026-03-15 03:30:09 +0200

    Provided the cubits to the app

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,11 @@ import 'package:bookly/core/services/get_it_service.dart';
 import 'package:bookly/core/services/observer.dart';
 import 'package:bookly/core/utils/app_router.dart';
 import 'package:bookly/features/home/domain/entities/book_entity.dart';
+import 'package:bookly/features/home/domain/repos/home_repo.dart';
+import 'package:bookly/features/home/domain/use_cases/fetch_featured_books_use_case.dart';
+import 'package:bookly/features/home/domain/use_cases/fetch_newest_books_use_case.dart';
+import 'package:bookly/features/home/presentation/managers/fetch_featured_books_cubit/fetch_featured_books_cubit.dart';
+import 'package:bookly/features/home/presentation/managers/fetch_newest_books_cubit/fetch_newest_books_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -24,16 +29,30 @@ class BooklyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: AppRouter.router,
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        scaffoldBackgroundColor: kPrimaryColor,
-        textTheme: GoogleFonts.montserratTextTheme(
-          ThemeData.dark().textTheme,
-        ).apply(bodyColor: Colors.white, displayColor: Colors.white),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => FetchFeaturedBooksCubit(
+            FetchFeaturedBooksUseCase(homeRepo: getIt.get<HomeRepo>()),
+          )..fetchFeaturedBooks(),
+        ),
+        BlocProvider(
+          create: (context) => FetchNewestBooksCubit(
+            FetchNewestBooksUseCase(homeRepo: getIt.get<HomeRepo>()),
+          )..fetchNewestBooks(),
+        ),
+      ],
+      child: MaterialApp.router(
+        routerConfig: AppRouter.router,
+        theme: ThemeData(
+          brightness: Brightness.dark,
+          scaffoldBackgroundColor: kPrimaryColor,
+          textTheme: GoogleFonts.montserratTextTheme(
+            ThemeData.dark().textTheme,
+          ).apply(bodyColor: Colors.white, displayColor: Colors.white),
+        ),
+        debugShowCheckedModeBanner: false,
       ),
-      debugShowCheckedModeBanner: false,
     );
   }
 }
