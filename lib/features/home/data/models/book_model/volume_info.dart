f8e9908da390e87bff1508cfe42686a8bbf6c0bf
Sherif80 <sherifashraf1054@gmail.com>
2026-03-15 03:42:12 +0200

    fixed a bug with casting 2 lists of strings to dynamic

--- lib/features/home/data/models/book_model/volume_info.dart
+++ lib/features/home/data/models/book_model/volume_info.dart
@@ -52,7 +52,9 @@ class VolumeInfo {
 
   factory VolumeInfo.fromJson(Map<String, dynamic> json) => VolumeInfo(
     title: json['title'] as String?,
-    authors: json['authors'] as List<String>?,
+    authors: (json['authors'] as List<dynamic>?)
+        ?.map((e) => e as String)
+        .toList(),
     publisher: json['publisher'] as String?,
     publishedDate: json['publishedDate'] as String?,
     description: json['description'] as String?,
@@ -64,7 +66,9 @@ class VolumeInfo {
         : ReadingModes.fromJson(json['readingModes'] as Map<String, dynamic>),
     pageCount: json['pageCount'] as int?,
     printType: json['printType'] as String?,
-    categories: json['categories'] as List<String>?,
+    categories: (json['categories'] as List<dynamic>?)
+        ?.map((e) => e as String)
+        .toList(),
     averageRating: json['averageRating'] as num?,
     ratingsCount: json['ratingsCount'] as num?,
     maturityRating: json['maturityRating'] as String?,
